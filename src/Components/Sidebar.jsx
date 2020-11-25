fb9fc75f370776c696a303b46357f3e43c78dfe5
Larisse <larisse.lima2@gmail.com>
2020-11-25 15:33:54 -0300

    Adiciona componente Sidebar

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import '../Styles/Components/sidebar.css';
+import Nav from '../Components/Nav/Nav'
+
+export default function Sidebar() {
+    return (
+        <aside className="app-sidebar">
+            <Nav />
+        </aside>
+    )
+
+}
