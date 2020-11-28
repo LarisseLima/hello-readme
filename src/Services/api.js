3f5ae37e7e2148cca8e1ed267417dd4943fcea0e
Larisse <larisse.lima2@gmail.com>
2020-11-27 21:36:59 -0300

    Adicionando services

---
+++ src/Services/api.js
@@ -0,0 +1,85 @@
+[
+	{
+		"type": "artigo",
+		"title": "Como fazer um bom README",
+		"link": "https://blog.rocketseat.com.br/como-fazer-um-bom-readme/"
+
+	},
+	{
+		"type": "artigo",
+		"title": "Como criar um README incrível para seus projetos (e perfil) no Github",
+		"link": "https://onebitcode.com/como-criar-um-readme-incrivel-para-seus-projetos-e-perfil-no-github/"
+
+	},
+	{
+		"type": "artigo",
+		"title": "Como escrever um README.md sensacional no Github",
+		"link": "https://dev.to/reginadiana/como-escrever-um-readme-md-sensacional-no-github-4509"
+
+	},
+	{
+		"type": "artigo",
+		"title": "Readme MD: tudo o que você precisa saber",
+		"link": "https://uniconverter.wondershare.com.br/convert-other-format/readme-md.htmlt"
+
+	},
+	{
+		"type": "artigo",
+		"title": "Criando um README para seu perfil do GitHub",
+		"link": "https://www.treinaweb.com.br/blog/criando-um-readme-para-seu-perfil-do-github/"
+
+	},
+	{
+		"type": "artigo",
+		"title": "Criando anotações com Markdown",
+		"link": "https://www.alura.com.br/artigos/criando-anotacoes-com-markdown"
+	},
+	{
+		"type": "artigo",
+		"title": "Qual é a importância do README.md no Git ?",
+		"link": "https://pt.stackoverflow.com/questions/297006/qual-%C3%A9-a-import%C3%A2ncia-do-readme-md-no-git"
+	},
+	{
+		"type": "templates",
+		"title": "Make a README",
+		"link": "https://www.makeareadme.com/"
+
+	},
+	{
+		"type": "templates",
+		"title": "Readme Template",
+		"link": "https://github.com/dbader/readme-template"
+
+	},
+	{
+		"type": "templates",
+		"title": "Best README Template",
+		"link": "https://github.com/othneildrew/Best-README-Template"
+	},
+	{
+		"type": "templates",
+		"title": "Stackedit",
+		"link": "https://stackedit.io/app#"
+
+	},
+	{
+		"type": "vídeo",
+		"title": "Como escrever e personalizar um README no Github",
+		"link": "https://www.youtube.com/watch?v=2A_ebBA3jzM"
+
+	},
+	{
+		"type": "vídeo",
+		"title": "Como criar um README",
+		"link": "https://www.youtube.com/watch?v=Gcb60rPbnKA&t=930s"
+
+	},
+	{
+		"type": "vídeo",
+		"title": "Dicas para seu Profile README.md do Github",
+		"link": "https://www.youtube.com/watch?v=5jxQtrW_p24"
+
+	}
+
+
+]
