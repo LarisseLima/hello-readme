acaa7fd860d6bc6bc53d21e8b686ec8c5541748c
Larisse <larisse.lima2@gmail.com>
2020-11-29 12:55:09 -0300

    Adiciona logo na Nav

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import Logo from '../../assets/logo.svg'
 import '../../Styles/Components/nav.css'
 
 export default function Nav() {
+  const path = useLocation().pathname;
   return (
-    <div>
+    <div class="nav__wrapper">
+      { path !== '/' && <img className="nav__logo" src={Logo} alt="Logo da barra de navegação" />}
       <nav className="nav__container">
         <ul className='nav__links'>
           <li>
             <Link to="/"
-              className="link" >
+              className="link">
               Home
               </Link>
           </li>
           <li>
             <Link to="tips"
-              className="link">
+              className={path === '/tips' ? 'selected-link link' : 'link'} >
               Dicas
               </Link>
           </li>
           <li>
             <Link to="links"
-              className="link">
+              className={path === '/links' ? 'selected-link link' : 'link'} >
               Links
                 </Link>
           </li>
