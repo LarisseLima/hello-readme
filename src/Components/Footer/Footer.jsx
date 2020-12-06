d197784f59476ab9aab0d4831ac3299e1339fe03
Larisse <larisse.lima2@gmail.com>
2020-12-05 21:30:25 -0300

    Ajustes css footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,23 +2,27 @@ import React from 'react';
 import { AiFillLinkedin } from 'react-icons/ai';
 import { AiFillGithub } from 'react-icons/ai';
 import { MdEmail } from 'react-icons/md';
+import { useLocation } from 'react-router-dom'
 import './footer.css';
 
 export default function Footer() {
+  const path = useLocation().pathname;
   return (
-    <footer className="footer-bio">
-      <a href="https://www.linkedin.com/in/larisselima/" className="icone-redes-in"
-        target="blank">
-        <AiFillLinkedin />
-      </a>
-      <a href="https://github.com/LarisseLima/hello-readme" className="icone-redes-gi"
-        target="blank">
-        <AiFillGithub />
-      </a>
-      <a href="mailto:larisse.lima2@gmail.com" className="icone-redes-em"
-        target="blank">
-        <MdEmail />
-      </a>
-    </footer>
+    <div className={path !== '/' ? 'footer__wrapper' : ''}>
+      <footer className="foote__bio">
+        <a href="https://www.linkedin.com/in/larisselima/" className="icone-redes-in"
+          target="blank">
+          <AiFillLinkedin />
+        </a>
+        <a href="https://github.com/LarisseLima/hello-readme" className="icone-redes-gi"
+          target="blank">
+          <AiFillGithub />
+        </a>
+        <a href="mailto:larisse.lima2@gmail.com" className="icone-redes-em"
+          target="blank">
+          <MdEmail />
+        </a>
+      </footer>
+    </div>
   )
 }
